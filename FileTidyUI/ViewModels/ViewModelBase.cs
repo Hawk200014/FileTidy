8347df34bf0ad6aef34580f6f7b6391ef65bb9be
Hawk_200014 <damienbecker20@gmail.com>
2025-03-09 14:32:11 +0100

    frist commit

---
+++ FileTidyUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace FileTidyUI.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ FileTidyUI/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace FileTidyUI.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
