f3300e729a0ea3c0cda3dc1dc914709ddfe9820d
Hawk200014 <72735051+Hawk200014@users.noreply.github.com>
2025-04-27 23:17:28 +0200

    Added SortFolder Dialog Removed Placeholder Numbers to file count and index

---
+++ FileTidyUI/Views/Dialogs/SortFolderDialog.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace FileTidyUI.Views.Dialogs;
+
+public partial class SortFolderDialog : Window
+{
+    public SortFolderDialog()
+    {
+        InitializeComponent();
+    }
+}
